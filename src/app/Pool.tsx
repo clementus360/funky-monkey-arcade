c28fe97918c36b7d95a50c430d4fe7c823646a51
clementus360 <clementus360@gmail.com>
2024-02-23 00:21:48 +0200

    escape quote

--- src/app/Pool.tsx
+++ src/app/Pool.tsx
@@ -8,7 +8,7 @@ export default function Pool() {
 
             <div className='flex flex-col gap-4 px-8 sm:pl-0'>
                 <h2 className='font-title font-bold text-white text-3xl sm:textvw-[40] lineheight-[16]'><span className='text-primary'>DOUBLE THE FUN </span><br />AT OUR POOL TABLES</h2>
-                <p className='font-text text-xl sm:textvw-[20] text-text lineheight-[26]'>From intense matches to laid-back rounds with friends, our pool tables cater to diverse preferences and skill levels. So, if you're ready to break into a new level of fun, grab your cue and join us for an unforgettable gaming experience!</p>
+                <p className='font-text text-xl sm:textvw-[20] text-text lineheight-[26]'>From intense matches to laid-back rounds with friends, our pool tables cater to diverse preferences and skill levels. So, if you&rsquo;re ready to break into a new level of fun, grab your cue and join us for an unforgettable gaming experience!</p>
             </div>
 
         </section>
